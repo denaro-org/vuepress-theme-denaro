0c62ab373f1c4c9e16e971eafd404eb1eafc7ec9
vxhly <pengchengou@gmail.com>
2022-07-11 08:50:47 +0800

    fix(plugin-moefy-canvas,plugin-vue-preview): 修复插件样式

--- packages/@vuepress-denaro/plugin-qrcode/src/client/components/const.ts
+++
@@ -1,7 +0,0 @@
-declare const __QRCODE_LABEL_TEXT__: string | Record<string, string>
-declare const __QRCODE_SIZE__: number | 'small' | 'medium' | 'big'
-declare const __QRCODE_CHANNEL__: boolean
-
-export const labelText = __QRCODE_LABEL_TEXT__
-export const size = __QRCODE_SIZE__
-export const channel = __QRCODE_CHANNEL__
