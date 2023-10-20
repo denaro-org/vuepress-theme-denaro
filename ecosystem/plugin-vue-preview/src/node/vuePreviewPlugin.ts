97f726bd82ba71cf36b4893ec6748414354f1560
vxhly <pengchengou@gmail.com>
2023-10-20 22:14:34 +0800

    refactor: 💡 plugin-vue-preview
    修改或优化客户端代码

--- ecosystem/plugin-vue-preview/src/node/vuePreviewPlugin.ts
+++ ecosystem/plugin-vue-preview/src/node/vuePreviewPlugin.ts
@@ -1,6 +1,5 @@
-import { hash, path } from '@vuepress/utils'
 import type { PluginFunction } from '@vuepress-denaro/core'
-import { useDenaroPlugin } from '@vuepress-denaro/core'
+import { hash, path, useDenaroPlugin } from '@vuepress-denaro/core'
 import type { VuePreviewOptions } from '../shared/index.js'
 import { mdPluginVuePreview } from './md-plugin-vue-preview/index.js'
 import { prepareConfigFile } from './utils/prepareConfigFile.js'
